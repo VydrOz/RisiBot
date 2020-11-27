1be75ae20e3caf987c2785088700e200198805e8
Vydro <61025448+Vydro@users.noreply.github.com>
2020-11-27 14:08:46 +0100

    fix multi args and update command run

--- events/message.js
+++ events/message.js
@@ -1,18 +1,16 @@
 exports.run = (client, msg) => {
 	if (msg.author.bot) return;
 	if (msg.channel.type !== 'text') return msg.reply('You must run the commands on a Discord server on which the Discord Bot is available');
-	    const prefix = client.guildconfs.get(msg.guild.id).prefix;
+
+	const prefix = client.guildconfs.get(msg.guild.id).prefix;
 	if (!msg.content.startsWith(prefix)) return;
     
-	const args = msg.content.slice(prefix.length).trim().split(' ');
+	const args = msg.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
-	let cmd;
-	if (client.commands.has(command)) {
-	    cmd = client.commands.get(command);
-    } else if (client.aliases.has(command)) {
-	    cmd = client.commands.get(client.aliases.get(command));
-    }
-	if (cmd) {
-	    cmd.run(client, msg, args);
-    }
+	if (!client.commands.has(command)) return;	
+	try {
+		client.commands.get(command).run(client, msg, args);
+	} catch (error) {
+		console.error(error);
+	}
 };
